05a03dd274ed4259a0518d7919faea3089322ddc
zieeco <zieecosam@gmail.com>
2022-05-03 15:02:06 +0100

    Add the can_be_achieved method and update the initialize constructor

--- src/item.rb
+++ src/item.rb
@@ -1,10 +1,16 @@
 class Item
   attr_accessor :genre, :label, :source, :author, :publish_date
 
-  def initialize(name, publish_date, archived = false )
+  def initialize(name, publish_date, archived: false)
     @id = Random.rand(1..1000)
     @publish_date = publish_date
     @name = name
     @archived = archived
+  end
+
+  private
+
+  def can_be_achieved?
+    return true if @publish_date.year < Time.new.year - 10
   end
 end
