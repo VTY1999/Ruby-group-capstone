7fb7d4dad1f4dfe16c16965936f73b6036f13f33
zieeco <zieecosam@gmail.com>
2022-05-04 07:02:56 +0100

    Add game class

---
+++ src/game.rb
@@ -0,0 +1,11 @@
+class Game
+  attr_accessor :multiplayer, :last_played_at
+
+  # rubocop:disable Metrics/ParameterLists
+  def initialize(name:, publish_date:, multiplayer:, last_played_at:, id: Random.rand(1...1000), archived: false)
+    super(name: name, publish_date: publish_date, id: id, archived: archived)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+  # rubocop:enable Metrics/ParameterLists
+end
