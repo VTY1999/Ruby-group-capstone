21c9036e14c9ed410efb91835e3791ee59e09f1c
verissimo francis <verissimotoyin50@gmail.com>
2022-05-05 16:06:43 +0100

    Update to item, movie, and source file

--- src/movie.rb
+++ src/movie.rb
@@ -1,16 +1,17 @@
+require 'date'
 require_relative './item'
 
 class Movie < Item
   attr_accessor :silent
 
-  def initialize(silent:, published_date:, id: Random.rand(1...1000))
-    super(id, published_date)
+  def initialize(silent, publish_date)
+    super(publish_date, archived)
     @silent = silent
   end
 
-  private
-
   def can_be_archived?
-    super || @silent
+    return true if super && @silent
   end
+
+  private :can_be_archived?
 end
